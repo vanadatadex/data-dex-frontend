1c8bb93f1ca42d0ca14827dd95f15bc5256a7637
stephen <991266+stxphxn@users.noreply.github.com>
2024-12-07 20:35:22 +0000

    feat: add wallet connect

--- src/constants/chainInfo.ts
+++ src/constants/chainInfo.ts
@@ -293,7 +293,7 @@ export function getChainInfo(chainId: any): any {
   return undefined
 }
 
-const MAINNET_INFO = CHAIN_INFO[ChainId.MAINNET]
+const MAINNET_INFO = CHAIN_INFO[ChainId.VANA]
 export function getChainInfoOrDefault(chainId: number | undefined) {
   return getChainInfo(chainId) ?? MAINNET_INFO
 }
