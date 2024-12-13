2bd9597f2a0a5c76e91bc2f6d3c1a41e63ae7a0b
stephen <991266+stxphxn@users.noreply.github.com>
2024-12-13 17:17:43 +0000

    feat: update vana icon

--- src/hooks/useStablecoinPrice.ts
+++ src/hooks/useStablecoinPrice.ts
@@ -27,7 +27,7 @@ const STABLECOIN_AMOUNT_OUT: { [chainId: number]: CurrencyAmount<Token> } = {
   [ChainId.CELO]: CurrencyAmount.fromRawAmount(CUSD_CELO, 10_000e18),
   [ChainId.BNB]: CurrencyAmount.fromRawAmount(USDT_BSC, 100e18),
   [ChainId.AVALANCHE]: CurrencyAmount.fromRawAmount(USDC_AVALANCHE, 10_000e6),
-  [ChainId.VANA]: CurrencyAmount.fromRawAmount(USDC_VANA, 1e18),
+  [ChainId.VANA]: CurrencyAmount.fromRawAmount(USDC_VANA, 1e6),
   [ChainId.VANA_MOKSHA]: CurrencyAmount.fromRawAmount(USDC_VANA_MOKSHA, 1e6),
 }
 
